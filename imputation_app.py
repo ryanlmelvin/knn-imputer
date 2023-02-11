6bada23e8ae0495b0cd32b12bdbdb01ff0e05aca
Ryan L. Melvin <80171583+ryanlmelvin@users.noreply.github.com>
2023-02-11 12:48:55 -0600

    Update imputation_app.py

--- imputation_app.py
+++ imputation_app.py
@@ -2,24 +2,26 @@ import streamlit as st
 import pandas as pd
 from sklearn.impute import KNNImputer
 
-def impute_missing_values_knn(df, column, k):
-    knn_imputer = KNNImputer(n_neighbors=k)
-    df[column] = knn_imputer.fit_transform(df[[column]])
+@st.cache
+def impute_missing_values(df, column, k):
+    imputer = KNNImputer(n_neighbors=k)
+    df[column] = imputer.fit_transform(df[[column]])
     return df
 
 def main():
-    st.title("Imputing Missing Values with kNN")
-    uploaded_file = st.file_uploader("Upload an Excel Spreadsheet", type=["xlsx", "xls"])
-
+    st.title("Imputation App")
+    uploaded_file = st.file_uploader("Upload your Excel spreadsheet", type=["xlsx"])
     if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)
-        st.dataframe(df.head())
-
-        column = st.selectbox("Select a column from the spreadsheet", df.columns)
-        k = st.number_input("Enter the number of neighbors (k)", min_value=1, value=5)
-        if st.button("Impute Missing Values"):
-            df = impute_missing_values_knn(df, column, k)
-            st.dataframe(df.head())
+        st.write("Data Preview:", df.head())
+        column = st.selectbox("Select a column", df.columns)
+        k = st.number_input("Enter the value of k", min_value=1, value=1)
+        if st.button("Impute"):
+            df = impute_missing_values(df, column, k)
+            st.write("Imputed Data Preview:", df.head())
+            if st.button("Download"):
+                df.to_excel("imputed_data.xlsx", index=False)
+                st.success("Data has been downloaded successfully!")
 
 if __name__ == "__main__":
     main()
